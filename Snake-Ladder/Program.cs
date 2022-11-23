08f15a47012b35e363befa01f2737b47169a0d1f
karanmaurya15 <karanmaurya9663@gmail.com>
2022-11-23 20:38:31 +0530

    Player start the Game

--- Snake-Ladder/Program.cs
+++ Snake-Ladder/Program.cs
@@ -5,6 +5,11 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Wellcome to Snake And Ladder Program");
+            int PlayerPosition = 0;
+            Console.WriteLine("\nPress enter to Play the game");
+            string play = Console.ReadLine();
+
+            Console.WriteLine("Player is at Start position  : " + PlayerPosition);
         }
     }
 }
