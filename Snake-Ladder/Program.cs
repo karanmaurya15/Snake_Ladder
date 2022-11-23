e037d12a39b9c016b8eb56468e96286ca7b9190d
karanmaurya15 <karanmaurya9663@gmail.com>
2022-11-23 21:35:29 +0530

    No Play, Ladder or Snake condition  is Done

--- Snake-Ladder/Program.cs
+++ Snake-Ladder/Program.cs
@@ -5,14 +5,35 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Wellcome to Snake And Ladder Program");
+            int StartingPosition = 0;
             int PlayerPosition = 0;
             int diceRoll; 
             Console.WriteLine("\nPress enter to Play the game");
             string play = Console.ReadLine();
-            Console.WriteLine("Player is at Start position : " + PlayerPosition);
+            
             Random random = new Random();
             diceRoll = random.Next(1, 7);
-            Console.WriteLine("\nPlayer rolled number is : " + diceRoll);
+            Console.WriteLine($"Player rolled number {diceRoll} on position {PlayerPosition}\n");
+
+            int option = random.Next(1, 3);
+            switch (option)
+            {
+                case 1:
+                    Console.WriteLine("Player landed on a Ladder");
+                    PlayerPosition += diceRoll;
+                    Console.WriteLine($"Player moves to position {PlayerPosition}");
+                    break;
+                case 2:
+                    Console.WriteLine("Player landed on a Snake");
+                    PlayerPosition -= diceRoll;
+                    Console.WriteLine($"Player moves to position {PlayerPosition}");
+                    if (PlayerPosition < 0)
+                    {
+                        Console.WriteLine("No Play, Try Again");
+                        PlayerPosition = 0;
+                    }
+                    break;
+            }
         }
     }
 }
