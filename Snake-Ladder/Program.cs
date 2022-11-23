c8b3328de619162f3f7c868054ca9dbe20edea20
karanmaurya15 <karanmaurya9663@gmail.com>
2022-11-23 20:53:15 +0530

    Rolling of Die is Done

--- Snake-Ladder/Program.cs
+++ Snake-Ladder/Program.cs
@@ -6,10 +6,13 @@
         {
             Console.WriteLine("Wellcome to Snake And Ladder Program");
             int PlayerPosition = 0;
+            int diceRoll; 
             Console.WriteLine("\nPress enter to Play the game");
             string play = Console.ReadLine();
-
-            Console.WriteLine("Player is at Start position  : " + PlayerPosition);
+            Console.WriteLine("Player is at Start position : " + PlayerPosition);
+            Random random = new Random();
+            diceRoll = random.Next(1, 7);
+            Console.WriteLine("\nPlayer rolled number is : " + diceRoll);
         }
     }
 }
