0de366c237d3d62ab00e96886f60adc117294666
karanmaurya15 <karanmaurya9663@gmail.com>
2022-11-24 22:00:54 +0530

    Player reaches the Winning Position

--- Snake-Ladder/Program.cs
+++ Snake-Ladder/Program.cs
@@ -5,35 +5,41 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Wellcome to Snake And Ladder Program");
-            int StartingPosition = 0;
+            int StartPosition = 0;
             int PlayerPosition = 0;
+            int  WinningPosition = 100;
             int diceRoll; 
             Console.WriteLine("\nPress enter to Play the game");
             string play = Console.ReadLine();
-            
-            Random random = new Random();
-            diceRoll = random.Next(1, 7);
-            Console.WriteLine($"Player rolled number {diceRoll} on position {PlayerPosition}\n");
-
-            int option = random.Next(1, 3);
-            switch (option)
+          
+            while (PlayerPosition >= StartPosition && PlayerPosition <= WinningPosition)
             {
-                case 1:
-                    Console.WriteLine("Player landed on a Ladder");
-                    PlayerPosition += diceRoll;
-                    Console.WriteLine($"Player moves to position {PlayerPosition}");
-                    break;
-                case 2:
-                    Console.WriteLine("Player landed on a Snake");
-                    PlayerPosition -= diceRoll;
-                    Console.WriteLine($"Player moves to position {PlayerPosition}");
-                    if (PlayerPosition < 0)
-                    {
-                        Console.WriteLine("No Play, Try Again");
-                        PlayerPosition = 0;
-                    }
-                    break;
+                Random random = new Random();
+                diceRoll = random.Next(1, 7);
+               
+                Console.WriteLine("The Position of Player is " + PlayerPosition + " " + "with a roll of " + diceRoll);
+                int option = random.Next(1, 3);
+                switch (option)
+                {
+                    case 1:
+                        Console.WriteLine("\nPlayer landed on a Ladder");
+                        PlayerPosition += diceRoll;
+                       Console.WriteLine($"Player moves to position {PlayerPosition}");
+                        break;
+                    case 2:
+                        Console.WriteLine("\nPlayer landed on a Snake");
+                        PlayerPosition -= diceRoll;
+                        Console.WriteLine($"Player moves to position {PlayerPosition}");
+                        if (PlayerPosition < 0)
+                        {
+                            Console.WriteLine("\nNo Play, Try Again");
+                            PlayerPosition = 0;
+                        }
+                        break;
+                }
+               //  Console.WriteLine("The Position of Player is " + PlayerPosition );
             }
+            Console.WriteLine("\nThe Player wins the game");
         }
     }
 }
