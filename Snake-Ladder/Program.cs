378c8337453520f6e35af82cfcbf06d1459e2f44
karanmaurya15 <karanmaurya9663@gmail.com>
2022-11-26 20:20:15 +0530

    Number of times dice played

--- Snake-Ladder/Program.cs
+++ Snake-Ladder/Program.cs
@@ -5,10 +5,11 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Wellcome to Snake And Ladder Program");
-            int StartPosition = 0;
+           
             int PlayerPosition = 0;
             int  WinningPosition = 100;
-            int diceRoll; 
+            int diceRoll;
+            int diceRollCount = 0;
             Console.WriteLine("\nPress enter to Play the game");
             string play = Console.ReadLine();
             Console.WriteLine($"Player is at position {PlayerPosition}");
@@ -19,11 +20,12 @@
                 diceRoll = random.Next(1, 7);
 
                 Console.WriteLine($"\nPlayer rolled number {diceRoll} on position {PlayerPosition}");
+                diceRollCount++;
                 int option = random.Next(1, 3);
                 switch (option)
                 {
                     case 1:
-                        Console.WriteLine("\nPlayer landed on a Ladder");
+                        Console.WriteLine("Player landed on a Ladder");
                         PlayerPosition += diceRoll;
                        Console.WriteLine($"Player moves to position {PlayerPosition}");
                         if (PlayerPosition > WinningPosition)
@@ -33,7 +35,7 @@
                         }
                         break;
                     case 2:
-                        Console.WriteLine("\nPlayer landed on a Snake");
+                        Console.WriteLine("Player landed on a Snake");
                         PlayerPosition -= diceRoll;
                         Console.WriteLine($"Player moves to position {PlayerPosition}");
                         if (PlayerPosition < 0)
@@ -48,6 +50,7 @@
             if (PlayerPosition == 100)
             {
                 Console.WriteLine("==========Player Wins==========");
+                Console.WriteLine($"\nDice was played {diceRollCount} times to win the game");
             }
         }
     }
